2d6f01bf0d0f4482576fd9337a7c1bdc9f0462ed
Maedeh-Saeidi <saeidimdhw@gmail.com>
2024-01-15 14:51:00 +0330

    Condition Rendering using ternary conditional

--- react-learning/src/components/ConditionalComponent.jsx
+++ react-learning/src/components/ConditionalComponent.jsx
@@ -3,11 +3,15 @@ import Counter from "./Counter";
 import FunctionalCounter from "./FunctionalCounter";
 export default function ConditionalComponent() {
   const [display, setDisplay] = useState(false);
-  let output;
-  if (display) {
-    output = <h3>This is a coditional component</h3>;
-  } else {
-    output = <h3>Nothing to see here</h3>;
-  }
-  return <div>{output}</div>;
+  return display ?
+    <div>
+      <h3>
+      This is a coditional component
+      </h3>
+    </div> :
+    <div>
+      <h3>
+      Nothing to see here
+      </h3>
+     </div>;
 }
