3b8abf0896d5aa15865d5b9851f8bc1ff466c7e2
Maedeh-Saeidi <saeidimdhw@gmail.com>
2024-01-22 15:20:26 +0330

    Rendering array of objects

--- react-learning(second part)/src/components/Fruits.jsx
+++ react-learning(second part)/src/components/Fruits.jsx
@@ -1,8 +1,19 @@
 export default function Fruits() {
-  const fruits = ["Apple", "Mango", "Banana", "Orange", "Pineapple"]
+  //const fruits = ["Apple", "Mango", "Banana", "Orange", "Pineapple"]
+  const fruits = [
+    { name: "Apple", price: 10, emoji: "🍎" },
+    { name: "Mango", price: 7, emoji: "🥭" },
+    { name: "Banana", price: 2, emoji: "🍌" },
+    { name: "Orange", price: 5, emoji: "🍊" },
+    { name: "Pineapple", price: 8, emoji: "🍍" }
+  ]
   return (
     <>
-      {fruits.map(fruit => <li key={fruit}>{fruit}</li>)}
+      <ul>
+        {fruits.map(fruit => (
+          <li key={fruit}>{fruit.name}${fruit.price}{fruit.emoji}</li>
+        ))} 
+      </ul>
     </>
   )
 }
