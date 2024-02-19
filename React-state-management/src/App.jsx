0b3ccb895abfdf1f2ae9684d8567cbee74aab370
Maedeh-Saeidi <saeidimdhw@gmail.com>
2024-02-19 18:42:17 +0330

    Cleaning workspace

--- React-state-management/src/App.jsx
+++ React-state-management/src/App.jsx
@@ -1,48 +1,10 @@
 import { useState } from 'react'
-function NameList() {
-  const [list, setList] = useState(["Jack", "Jill", "John"]);
-  const [name, setName] = useState(() => "Jacky");
+import UseState from './components/UseState'
 
-  function onAddName() {
-    setList([...list, name]);
-    setName("");
-  }
-  return (
-    <div>
-      <ul>
-        {list.map((name) => (
-          <li key={name}>{name}</li>
-        ))}
-      </ul>
-      <input
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <button
-        onClick={onAddName}
-      >Add</button>
-    </div>
-  )
-}
-
-function Counter() {
-  const [count, setCount] = useState(10);
-  function addOne() {
-    setCount(count + 1);
-  }
-  return (
-    <div>
-      <button
-        onClick={addOne}
-      >Count = {count}</button>
-    </div>
-  )
-}
 function App() {
   return (
     <div>
-      <Counter></Counter>
-      <NameList></NameList>
+      <UseState></UseState>
     </div>
   )
 }
---
+++ React-state-management/src/components/UseState.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+function NameList() {
+  const [list, setList] = useState(["Jack", "Jill", "John"]);
+  const [name, setName] = useState(() => "Jacky");
+
+  function onAddName() {
+    setList([...list, name]);
+    setName("");
+  }
+  return (
+    <div>
+      <ul>
+        {list.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+      <input
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button
+        onClick={onAddName}
+      >Add</button>
+    </div>
+  )
+}
+
+function Counter() {
+  const [count, setCount] = useState(10);
+  function addOne() {
+    setCount(count + 1);
+  }
+  return (
+    <div>
+      <button
+        onClick={addOne}
+      >Count = {count}</button>
+    </div>
+  )
+}
+export default function UseState() {
+  return (
+    <div>
+      <Counter></Counter>
+      <NameList></NameList>
+    </div>
+  )
+}
