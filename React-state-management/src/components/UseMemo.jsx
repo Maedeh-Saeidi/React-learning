4a8155e7fc6873081299fe00184b5853566ee968
Maedeh-Saeidi <saeidimdhw@gmail.com>
2024-02-20 16:51:36 +0330

    useMemo hook

--- React-state-management/src/components/UseMemo.jsx
+++ React-state-management/src/components/UseMemo.jsx
@@ -7,17 +7,22 @@ export default function UseMemo() {
     () => numbers.reduce((acc, number) => acc + number, 0),
     [numbers]
   );
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
 
   const [names] = useState(["John", "Paul", "George", "Ringo"]);
+  const countTotal = count1 + count2;
 
   const sortedNames = useMemo(() => [...names].sort(), [names]);
 
-  console.log(total);
   return (
     <div>
       <div> Total : {total}</div>
       <div>Names: {names.join(", ")}</div>
       <div>SortedNames: {sortedNames.join(", ")}</div>
+      <button onClick={() => setCount1(count1 + 1)}>Count 1:{count1}</button>
+      <button onClick={() => setCount2(count2 + 1)}>Count 2: {count2}</button>
+      <div>Count Total: {countTotal}</div>
     </div>
   );
 }
