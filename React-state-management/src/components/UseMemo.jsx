31a81525c1c7d903ea8ed6e376572b53d5f32869
Maedeh-Saeidi <saeidimdhw@gmail.com>
2024-02-21 11:18:49 +0330

    useMemo and useCallback hook

--- React-state-management/src/components/UseMemo.jsx
+++ React-state-management/src/components/UseMemo.jsx
@@ -1,4 +1,14 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useCallback } from "react";
+
+function SortedList({ list, sortFunc }) {
+  console.log("hello motherfucker");
+  const sortedList = useMemo(() => {
+    console.log("Running Sort");
+    return [...list].sort(sortFunc), [list, sortFunc];
+  }, []);
+
+  return <div>{sortedList.join(",")}</div>;
+}
 
 export default function UseMemo() {
   const [numbers] = useState([10, 20, 30]);
@@ -7,19 +17,21 @@ export default function UseMemo() {
     () => numbers.reduce((acc, number) => acc + number, 0),
     [numbers]
   );
+  const [names] = useState(["John", "Paul", "George", "Ringo"]);
+
   const [count1, setCount1] = useState(0);
   const [count2, setCount2] = useState(0);
 
-  const [names] = useState(["John", "Paul", "George", "Ringo"]);
   const countTotal = count1 + count2;
 
   const sortedNames = useMemo(() => [...names].sort(), [names]);
+  const sortFunc = useCallback((a, b) => a.localeCompare(b) * -1, []);
 
   return (
     <div>
       <div> Total : {total}</div>
       <div>Names: {names.join(", ")}</div>
-      <div>SortedNames: {sortedNames.join(", ")}</div>
+      <SortedList list={names} sortFunc={sortFunc}></SortedList>
       <button onClick={() => setCount1(count1 + 1)}>Count 1:{count1}</button>
       <button onClick={() => setCount2(count2 + 1)}>Count 2: {count2}</button>
       <div>Count Total: {countTotal}</div>
