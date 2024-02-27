ae61549e7a5cbf11059b5b9ef83a82c3a190a00e
Maedeh-Saeidi <saeidimdhw@gmail.com>
2024-02-27 15:46:42 +0330

    React Router

--- React-Router/src/main.tsx
+++ React-Router/src/main.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import HomePage from './pages/HomePage.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+import ProfilePages from './pages/ProfilesPage.tsx'
+import NotFoundPage from './pages/NotFoundPage.tsx'
+import ProfilePage from './pages/ProfilePage.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage/>,
+    errorElement: <NotFoundPage/>
+  },
+  {
+    path: '/profiles',
+    element: <ProfilePages/>,
+     errorElement: <NotFoundPage/>,
+     children:[
+{
+    path:'/profiles/:profileId',
+    element: <ProfilePage/>
+  }
+     ]
+  },
+  
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HomePage />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
--- React-Router/src/pages/ProfilesPage.tsx
+++ React-Router/src/pages/ProfilesPage.tsx
@@ -1,9 +1,18 @@
+import { NavLink,Outlet } from "react-router-dom";
 export default function ProfilePages(){
 
   const Profiles = [1,2,3,4,5];
   return (
     <div>
-      <h1>Profiles Pages</h1>
+    <div className="flex flex-col gap-5 text-3xl p-[70px]">
+      {Profiles.map((profile)=>
+      <NavLink key={profile} to={`/profiles/${profile}`}
+     className={({ isActive }) => {
+              return isActive ? 'text-green-700' : '';
+            }}
+      >Profile {profile}</NavLink>)}
+    </div>
+    <Outlet/>
     </div>
   )
 }
