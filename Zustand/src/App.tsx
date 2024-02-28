9d6f3788487d8091dea579367c0f56653467d3a9
Maedeh-Saeidi <saeidimdhw@gmail.com>
2024-02-28 14:20:23 +0330

    Updating variable

--- Zustand/src/App.tsx
+++ Zustand/src/App.tsx
@@ -1,6 +1,23 @@
 import "./App.css";
+import { useCounterStore } from "./store";
 
 function App() {
-  return <h1 className="text-4xl font-bold underline">Hello world!</h1>;
+  const count = useCounterStore((state) => state.count);
+  return <OtherComponent count={count} />;
 }
+const OtherComponent = ({ count }: { count: number }) => {
+  const increment = useCounterStore((state) => state.increment);
+  const decrement = useCounterStore((state) => state.decrement);
+
+  return (
+    <div>
+      {count}
+      <div>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+      </div>
+    </div>
+  );
+};
+
 export default App;
