af1986038f571bf630b6f6c44c8a569817ba85d9
Maedeh-Saeidi <saeidimdhw@gmail.com>
2024-02-28 15:06:16 +0330

    Async function

--- Zustand/src/App.tsx
+++ Zustand/src/App.tsx
@@ -1,19 +1,29 @@
+import { useEffect } from "react";
 import "./App.css";
 import { useCounterStore } from "./store";
+
+const logCount = () => {
+  const count = useCounterStore.getState().count;
+  console.log("count", count);
+};
 
 function App() {
   const count = useCounterStore((state) => state.count);
   return <OtherComponent count={count} />;
 }
 const OtherComponent = ({ count }: { count: number }) => {
-  const increment = useCounterStore((state) => state.increment);
+  const incrementAsync = useCounterStore((state) => state.increment);
   const decrement = useCounterStore((state) => state.decrement);
+
+  useEffect(() => {
+    logCount();
+  }, []);
 
   return (
     <div>
       {count}
       <div>
-        <button onClick={increment}>Increment</button>
+        <button onClick={incrementAsync}>IncrementAsync</button>
         <button onClick={decrement}>Decrement</button>
       </div>
     </div>
