cfbf67a857f14406a1297e5ddcedae6e1342d168
Maedeh <Maedeh@NGN.local>
2024-04-08 16:52:20 +0330

    Advanced props

--- TypeScript-Tutorial/src/App.tsx
+++ TypeScript-Tutorial/src/App.tsx
@@ -3,6 +3,11 @@ import "./styles.css";
 import Greet from "./components/Greet";
 import Person from "./components/Person";
 import PersonList from "./components/PersonList";
+import Status from "./components/Status";
+import Heading from "./components/Heading";
+import Oscar from "./components/Oscar";
+import Button from "./components/Button";
+import Input from "./components/Input";
 
 function App() {
   const personName = {
@@ -31,9 +36,20 @@ function App() {
 
   return (
     <div>
-      <Greet name="maedeh" messageCounst={20} isLoggedIn={false} />
+      <Greet name="maedeh" isLoggedIn={true} />
       <Person name={personName} />
       <PersonList names={nameList} />
+      <Status status="error" />
+      <Heading>PlaceHolder</Heading>
+      <Oscar>
+        <Heading>Oscar goes to ...</Heading>
+      </Oscar>
+      <Button
+        handleClick={(event, id) => {
+          console.log("Button clicked", event, id);
+        }}
+      />
+      <Input value=" " handleChange={(event) => console.log(event)}></Input>
     </div>
   );
 }
--- TypeScript-Tutorial/src/components/Greet.tsx
+++ TypeScript-Tutorial/src/components/Greet.tsx
@@ -1,15 +1,17 @@
 type GreetProps = {
   name: string;
-  messageCounst: number;
+  messageCounst?: number;
   isLoggedIn: boolean;
 };
 
 export default function Greet(props: GreetProps) {
+  const { messageCounst = 0 } = props;
+
   return (
     <div>
       <h2>
         {props.isLoggedIn
-          ? `Welcome ${props.name}! You have ${props.messageCounst} unread messages`
+          ? `Welcome ${props.name}! You have ${messageCounst} unread messages`
           : `Welcome Guest`}
       </h2>
     </div>
