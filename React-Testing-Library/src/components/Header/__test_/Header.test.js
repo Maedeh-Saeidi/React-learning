854d2d6c29fe87350319f9d86b7bb294b8b1924b
Maedeh-Saeidi <saeidimdhw@gmail.com>
2024-06-22 12:07:45 +0330

    Describe Block

--- React-Testing-Library/src/components/Header/__test_/Header.test.js
+++ React-Testing-Library/src/components/Header/__test_/Header.test.js
@@ -1,55 +1,57 @@
 import { render, screen } from "@testing-library/react";
 import Header from "../Header";
 
-//GET BY
-it("should render some text passed into title prop", async () => {
-  render(<Header title="My header" />);
-  const headingElement = screen.getByText(/My header/i);
-  expect(headingElement).toBeInTheDocument();
-});
+describe("Header", () => {
+  //GET BY
+  it("should render some text passed into title prop", async () => {
+    render(<Header title="My header" />);
+    const headingElement = screen.getByText(/My header/i);
+    expect(headingElement).toBeInTheDocument();
+  });
 
-// it("should render some blablabla", async () => {
-//   render(<Header title="My header" />);
-//   const headingElement = screen.getByRole("heading");
-//   expect(headingElement).toBeInTheDocument();
-// });
+  // it("should render some blablabla", async () => {
+  //   render(<Header title="My header" />);
+  //   const headingElement = screen.getByRole("heading");
+  //   expect(headingElement).toBeInTheDocument();
+  // });
 
-it("should render some blablabla", async () => {
-  render(<Header title="My header" />);
-  const headingElement = screen.getByRole("heading", { name: "My header" });
-  expect(headingElement).toBeInTheDocument();
-});
+  it("should render some blablabla", async () => {
+    render(<Header title="My header" />);
+    const headingElement = screen.getByRole("heading", { name: "My header" });
+    expect(headingElement).toBeInTheDocument();
+  });
 
-it("should render some blablabla", async () => {
-  render(<Header title="My header" />);
-  const headingElement = screen.getByTitle("Header");
-  expect(headingElement).toBeInTheDocument();
-});
+  it("should render some blablabla", async () => {
+    render(<Header title="My header" />);
+    const headingElement = screen.getByTitle("Header");
+    expect(headingElement).toBeInTheDocument();
+  });
 
-it("should render some blablabla", async () => {
-  render(<Header title="My header" />);
-  const headingElement = screen.getByTestId("header-1");
-  expect(headingElement).toBeInTheDocument();
-});
+  it("should render some blablabla", async () => {
+    render(<Header title="My header" />);
+    const headingElement = screen.getByTestId("header-1");
+    expect(headingElement).toBeInTheDocument();
+  });
 
-//FIND BY
+  //FIND BY
 
-it("should render same text passed into title prop", async () => {
-  render(<Header title="My Header" />);
-  const headingElement = await screen.findByText(/my header/i);
-  expect(headingElement).toBeInTheDocument();
-});
+  it("should render same text passed into title prop", async () => {
+    render(<Header title="My Header" />);
+    const headingElement = await screen.findByText(/my header/i);
+    expect(headingElement).toBeInTheDocument();
+  });
 
-//QueryBy
+  //QueryBy
 
-it("should not be dogs text in it", async () => {
-  render(<Header title="My Header" />);
-  const headingElement = screen.queryByText(/dogs/i);
-  expect(headingElement).not.toBeInTheDocument();
-});
+  it("should not be dogs text in it", async () => {
+    render(<Header title="My Header" />);
+    const headingElement = screen.queryByText(/dogs/i);
+    expect(headingElement).not.toBeInTheDocument();
+  });
 
-it("should i do know what", () => {
-  render(<Header title="My Header" />);
-  const headingElements = screen.getAllByRole("heading");
-  expect(headingElements.length).toBe(2);
+  it("should i do know what", () => {
+    render(<Header title="My Header" />);
+    const headingElements = screen.getAllByRole("heading");
+    expect(headingElements.length).toBe(2);
+  });
 });
--- React-Testing-Library/src/components/TodoFooter/__test_/TodoFooter.test.js
+++ React-Testing-Library/src/components/TodoFooter/__test_/TodoFooter.test.js
@@ -10,50 +10,54 @@ const MockTodoFooter = ({ numberOfIncompleteTasks }) => {
   );
 };
 
-test("should render the correct amount of incomplete tasks", async () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={5} />);
-  const paragraphElement = screen.getByText(/5 tasks left/i);
-  expect(paragraphElement).toBeInTheDocument();
+describe("TodoFooter", () => {
+  test("should render the correct amount of incomplete tasks", async () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={5} />);
+    const paragraphElement = screen.getByText(/5 tasks left/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
+
+  it("should render 'task' when the number of incomplete tasks is one", async () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
 });
 
-it("should render 'task' when the number of incomplete tasks is one", async () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).toBeInTheDocument();
-});
+describe("Practicing Assertions in todoFooter", () => {
+  it("should render blablabla", async () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toBeTruthy();
+  });
 
-it("should render blablabla", async () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).toBeTruthy();
-});
+  it("should render blablabla", async () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toBeVisible();
+  });
 
-it("should render blablabla", async () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).toBeVisible();
-});
+  it("should render blablabla", async () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toContainHTML("p");
+  });
 
-it("should render blablabla", async () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).toContainHTML("p");
-});
+  it("should render blablabla", async () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByTestId("para");
+    expect(paragraphElement).toHaveTextContent("1 task left");
+  });
 
-it("should render blablabla", async () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByTestId("para");
-  expect(paragraphElement).toHaveTextContent("1 task left");
-});
+  // it("should render blablabla", async () => {
+  //   render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  //   const paragraphElement = screen.getByTestId("para");
+  //   expect(paragraphElement).not.toBeVisible();
+  // });
 
-// it("should render blablabla", async () => {
-//   render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-//   const paragraphElement = screen.getByTestId("para");
-//   expect(paragraphElement).not.toBeVisible();
-// });
-
-it("should render blablabla", async () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByTestId("para");
-  expect(paragraphElement.textContent).toBe("1 task left");
+  it("should render blablabla", async () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByTestId("para");
+    expect(paragraphElement.textContent).toBe("1 task left");
+  });
 });
